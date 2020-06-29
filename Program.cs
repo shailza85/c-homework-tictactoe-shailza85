0ac214bf3d5b00b6f4fc7144e2a9f1db42fe325c
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-06-29 01:05:38 -0600

    Updated file with comments and citations.

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace c_homework_tictactoe_shailza85
 
         }
          // Introduction and game rules for the player.
-         static void Introduction()
+         static void Introduction()  // Introduction shows the begining welcome screen and rules to the player.
         {
             Console.WriteLine("Welcome to TIC TAC TOE!!!");
             Console.WriteLine("Please press any key to begin!");
@@ -38,7 +38,7 @@ namespace c_homework_tictactoe_shailza85
             Console.ReadKey();
             Console.Clear();
         }
-        static void GameBoard()  //Game Board design that will be shown on screen after game rules when user will press enter key .
+        static void GameBoard()  //Game Board design that will be shown on screen after game rules when user will press enter key . BoardArray is used to assign the position.
         {
             Console.Clear();
             Console.WriteLine("Player 1= X and Player 2= O.");
@@ -83,25 +83,25 @@ namespace c_homework_tictactoe_shailza85
                     TokenXorO(player,playerInput);
 
                 }                    
-                    GameBoard();
+                    GameBoard(); //This function displays the game board.
                     playTurn++;
 
-                // The below 3 functions will check if the user wins.
+                // The below 3 functions will check if the user wins horizontal/vertical/diagonal.
                     WinHorizontal();
                     WinVertical();
                     WinDiagonal();
 
-                if(playTurn == 10)  
+                if(playTurn == 10)  //when the value of turn will 10, the match will be drawn.
                 {
 
                   playDraw();
                 }
 
-                do   
+                do              // The loop plays each players turns and checks player's input on the board.
                 {
                     Console.WriteLine("Ready to play Player {0}: Its your chance to play.", player);
 
-                    try  // @link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/try-catch
+                    try         // try/catch statement prevents the loop to crash due to unhandled exception.
                     {
                             playerInput = Convert.ToInt32(Console.ReadLine());
                     }
@@ -157,8 +157,7 @@ namespace c_homework_tictactoe_shailza85
                 } while (!inputStatus);
 
             } while (true);
-            // The loop plays each players turns and checks user's input on the board.
-        
+                    
         } // Main method ends.
 
         public static void TokenXorO(int player, int playerInput)
@@ -173,7 +172,7 @@ namespace c_homework_tictactoe_shailza85
                 playerToken = 'O';
             }
 
-                // Switch statement will check player's input as player token X or O.
+                // Switch case statement will check player token X or O to array position.
             // @link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/switch
 
             switch (playerInput)  
@@ -214,13 +213,13 @@ namespace c_homework_tictactoe_shailza85
                 BoardArray [8] = playerToken;
                 break;
             }
-        }
+        } //End of TokenXorO function.
 
         public static void WinHorizontal()  // This methods will check if the player has win horizontally.
         {
             char[] playerTokens = {'X','O'};
 
-            foreach (char playertoken in playerTokens)
+            foreach (char playertoken in playerTokens) //foreach loop will check each value of token in the array positioned horizotally.
             {
                 if(((BoardArray[0] == playerToken) && (BoardArray[1] == playerToken)) && (BoardArray[2] == playerToken) ||
                   ((BoardArray[3] == playerToken) && (BoardArray[4] == playerToken)) && (BoardArray[5] == playerToken) ||
@@ -238,21 +237,21 @@ namespace c_homework_tictactoe_shailza85
                     {
                         Console.WriteLine("Yay congratulations!!! You are the winner, Player 2. You have played horizontally. Would you like to play again? Please press Y or N.");
                     }
-                    chkYorN();
+                    chkYorN(); //Function called to put user input to play. Y or N.
                    // Console.WriteLine("Press any key to reset the game board!!");
                     //Console.ReadKey();
-                    resetGame();
+                    resetGame(); //Game board reset.
                     break;
                 }
             }
-        }
+        } //End of WinHorizontal function.
 
 
         public static void WinVertical()  // This methods will check if the player has win vertically.
         {
             char[] playerTokens = {'X','O'};
 
-            foreach (char playertoken in playerTokens)
+            foreach (char playertoken in playerTokens) //foreach loop will check each value of token in the array positioned vertically.
             {
                 if(((BoardArray[0] == playerToken) && (BoardArray[3] == playerToken)) && (BoardArray[6] == playerToken) ||
                   ((BoardArray[1] == playerToken) && (BoardArray[4] == playerToken)) && (BoardArray[7] == playerToken) ||
@@ -270,21 +269,21 @@ namespace c_homework_tictactoe_shailza85
                     {
                         Console.WriteLine("Yay congratulations!!! You are the winner, Player 2. You have played vertically. Would you like to play again? Please press Y or N.");
                     }
-                    chkYorN();
+                    chkYorN(); //Function called to put user input to play. Y or N.
                     //Console.WriteLine("Press any key to reset the game board!!");
                     //Console.ReadKey();
-                    resetGame();
+                    resetGame(); //Game board reset. 
 
                     break;
                 }
             }
-        }
+        } //End of WinVertical function.
 
         public static void WinDiagonal()  // This methods will check if the player has win diagonally.
         {
             char[] playerTokens = {'X','O'};
 
-            foreach (char playertoken in playerTokens)
+            foreach (char playertoken in playerTokens)  //foreach loop will check each value of token in the array positioned diagonally.
             {
                 if(((BoardArray[0] == playerToken) && (BoardArray[4] == playerToken)) && (BoardArray[8] == playerToken) ||
                   ((BoardArray[6] == playerToken) && (BoardArray[4] == playerToken)) && (BoardArray[2] == playerToken) )
@@ -301,24 +300,23 @@ namespace c_homework_tictactoe_shailza85
                     {
                         Console.WriteLine("Yay congratulations!!! You win the game diagonally, Player 2. Would you like to play again? Please press Y or N.");
                     }
-                        chkYorN();
+                    chkYorN(); //Function called to put user input to play. Y or N.
                     //Console.WriteLine("Press any key to reset the game board!!");
                     //Console.ReadKey();
-                    resetGame();
+                    resetGame(); //Game board reset. 
                     break;
                 }
             } 
-        }
+        } //End of WinDiagonal function.
 
         public static void playDraw()  //playDraw method will prompt the user to play again as game is a draw.
         {         
             Console.WriteLine("The game is a Draw. Would you like to play again? Please press Y or N.");
-            //Console.ReadKey(); 
-            chkYorN();
-            resetGame();        
-        }
+            chkYorN(); //Function called to put user input to play. Y or N.
+            resetGame();   //Game board reset.     
+        } //End of playDraw function.
 
-        public static void chkYorN()
+        public static void chkYorN()  //This function checks the input value from player. If player wants to play, press Y. If don't, press N. If there is any other character entered, player will get and error and need to press Y/N.
         {
             char inputValue = '0';
             bool inputError = true;
@@ -326,27 +324,34 @@ namespace c_homework_tictactoe_shailza85
             while (inputError == true)
             {
                 inputValue = Console.ReadKey().KeyChar;
-                inputValue = inputValue.ToString().ToUpper().ToCharArray()[0];
+                inputValue = inputValue.ToString().ToUpper().ToCharArray()[0]; //input character Y/N will be coverted to Upper case.
 
-                if (inputValue == 'Y')
+                if (inputValue == 'Y')  //Input character 'Y'.
                 {
                     inputError = false;
                 }
 
-                else if (inputValue == 'N')
+                else if (inputValue == 'N') //Input character 'N'.
                 {
                      inputError = false;
                      Console.WriteLine("Thank you for playing the game. Enjoy your rest of the day!!!");
                 }
 
-            else
-            {
+              else
+              {
                 inputError = true;
-                Console.WriteLine("You have entered invalid character. please try again.");
-            }
+                Console.WriteLine("Error: You have entered invalid character. Please try again."); //Character enterd other than Y/N will give error message.
+              }
             }
 
-        }
+        } // End of chkYorN function.
     }
 
 }
+// code borrowed and got concept from follwing: 
+// @link https://daracrawford.com/new-blog-3/c-console-game-tic-tac-toe
+// @ link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/try-catch
+// @ link https://www.w3schools.com/cs/cs_switch.asp
+// @ link https://social.msdn.microsoft.com/Forums/vstudio/en-US/31b20379-42a0-45a4-8983-9d3b6be88be4/yn-prompt-c?forum=csharpgeneral
+// @link https://stackoverflow.com/questions/29297305/how-to-break-out-of-the-if-statement
+// @link https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays
